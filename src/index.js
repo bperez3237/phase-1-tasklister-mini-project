fb579584e22ea1b2355385c2e1482510d91d21e6
Brian <b.perez3237@gmail.com>
2022-02-17 19:47:49 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("#create-task-form").addEventListener('submit', (e) => {
+    e.preventDefault();
+    const ntd = document.getElementById("new-task-description");
+    const prio = document.getElementById("dropdown");
+    buildToDo(ntd.value, prio.value);
+  })
 });
+
+
+
+
+function buildToDo(todo, priority){
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'X'
+  p.textContent = `${todo} `;
+  if (priority === "high") {p.style.color = 'red'}
+  else if (priority === "med") {p.style.color = 'yellow'}
+  else if (priority === "low") {p.style.color = 'green' }
+  p.appendChild(btn);
+  document.querySelector('#tasks').appendChild(p);
+
+}
+
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
